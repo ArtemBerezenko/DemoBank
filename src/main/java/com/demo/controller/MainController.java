63bd9a66314ee8052d6f9ed908ddc6b96ee5d346
ArtemBerezenko <a.a.berezenko@gmail.com>
2017-08-13 13:15:05 +0600

    MainController

--- src/main/java/com/demo/controller/MainController.java
+++ src/main/java/com/demo/controller/MainController.java
@@ -1,4 +1,4 @@
-package hello;
+package com.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,15 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import model.User;
-import hello.UserRepository;
+import com.demo.model.User;
+import com.demo.repository.UserRepository;
 
 @Controller
 @RequestMapping(path="/demo")
 public class MainController {
+
     @Autowired
     private UserRepository userRepository;
 
+    @GetMapping(path="/add")
     public @ResponseBody String addNewUser (@RequestParam String login,
                                             @RequestParam String password) {
         User user = new User();
